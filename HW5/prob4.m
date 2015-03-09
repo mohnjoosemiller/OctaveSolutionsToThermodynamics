d5828a434561f559e5f5f4d7d92efcde626a21e0
John Moosemiller <itcaughtonj@gmail.com>
2015-03-09 04:24:56 -0500

    HW#5Q4solved

--- HW5/prob4.m
+++ HW5/prob4.m
@@ -2,6 +2,8 @@
 %John Moosemiller
 %Mar 9 2015
 
+%Assumptions: 1 mol gas enters
+
 %Constants
 R = 8.314
 Cp = 7/2*R
@@ -21,15 +23,32 @@ T2c = -25+273.15%convert C to K
 %exit stream (warm)
 P2w = 1
 T2w = 75+273.15%convert C to K
-%n2w = ?
+%n2w = 1-n2c
 
-%Adiabatic Q = 0
-%Ratio of Cold-Warm air
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %dU = Q+W
 %dU = dW
 %dU - dW = 0
 %CvdT - PdV = 0
 %Cv*(n2c*(T2c-T1)+n2w*(T2w-T1)) - (n2c*((R*T2c-R*T1)/(1-y))+n2w*((R*T2w-R*T1)/(1-y))) = 0
+%^^^^ NOTE: This above is incorrect... 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+
+%Adiabatic Q = 0
+%W = 0
+%Ratio of Cold-Warm air
+%dHw + dHc = 0
+%n2c*Cp*(T2c-T1)+(1-n2c)*Cp*(T2c-T1) = 0
+%n2c = -(dwarm/dcold) + n2c(dwarm/dcold) 
+n2c = (-((T2w-T1)/(T2c-T1))/(1-(T2w-T1)/(T2c-T1)))
+
+
+
+
+
+
+
 
 
 
