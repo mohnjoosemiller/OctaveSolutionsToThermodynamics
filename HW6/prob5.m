d2d28b9705891a7c75b9f8459bdd12801992b06c
John Moosemiller <itcaughtonj@gmail.com>
2015-03-31 02:13:28 -0500

    Updates with functions

--- HW6/prob5.m
+++ HW6/prob5.m
@@ -1,4 +1,4 @@
-%Problem 5 6.59
+%Problem 5 
 %John Moosemiller
 %Mar 29 2015
 
@@ -18,8 +18,14 @@ P0r = P0/Pc
 Pfr = Pf/Pc
 T0r = T0/Tc
 Tfr = Tf/Tc
-%Utilize eqns 6.91 
-
+%Utilize eqns 6.91 and 6.92 to find dH and dS respectively
+%we have dH = ICPH(T1,T2) + R*T(H2r - H1r)
+A = 3.931;B = 1.490*10^-3;C = 0;D = -.232*10^5;%from table C1
+dH = 8.314*ICPH(T0, Tf, A, B,C, D) + 8.314*Tc*(HRB(Tfr,Pfr,w) - HRB(T0r,P0r,w))
+dS = 8.314*(ICPS(T0,Tf,A,B,C,D)-log(Pf/P0)) + 8.314*(SRB(Tfr,Pfr,w)-SRB(T0r,P0r,w))
+%Yields
+%dH =  7407.3
+%dS =  1.8276
 
 
 
--- HW6/prob6.m
+++ HW6/prob6.m
@@ -5,7 +5,7 @@
 %n-butane either liquid or vapor
 %estimate V, H, and S at T = 370K
 %assume Pvap of n-butane is 1919.4kPa
-
+%6.54
 
 
 
