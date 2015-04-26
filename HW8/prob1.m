0cfea58597c9a1931bd5068e656de33dd451d774
John Moosemiller <itcaughtonj@gmail.com>
2015-04-26 15:29:53 -0500

    Update prob1.m

--- HW8/prob1.m
+++ HW8/prob1.m
@@ -4,3 +4,14 @@
 
 %steam enters turbine of pwerplant. - Rankine cycle
 %3300kPa 50kPa
+%inlet T of 450 550 650
+Plow = 50
+Phigh = 3300
+Hsatl50 = 340.564
+Hsatv50 = 2646
+Vsatl50 = 1.03
+Ssatl50 = 1.0912
+Ssatv50 = 7.5947
+%W=VdP
+Wpump = Vsatl50*(Phigh-Plow)
+Hstart = Hsatl50+Wpump
