8741ec3af004972e62f976a08fe1cee2cf2a51b8
John Moosemiller <itcaughtonj@gmail.com>
2015-04-27 18:38:48 -0500

    Update prob1.m

--- HW8/prob1.m
+++ HW8/prob1.m
@@ -15,3 +15,11 @@ Ssatv50 = 7.5947
 %W=VdP
 Wpump = Vsatl50*(Phigh-Plow)
 Hstart = Hsatl50+Wpump
+%from superheated steam tables at 450 550 and 650
+Hinterm = [3340.6,3565.3,3792.9]
+Sinterm = [7.0373,7.3282,7.5891]
+%find amount in each phase
+molfracvap = (Sinterm-Ssatl50)/(Ssatv50-Ssatl50)
+%Work = Hfinal-Hinitial
+Wturbine = Hsatl50+molfracvap*(Hsatv50-Hsatl50) - Hinterm
+n = (Wturbine+Wpump)/(Hinterm-Hstart)
